468f21364ca096d9031409f89e81b7c5d5ca0e14
seonyu <sunyu0216@naver.com>
2025-10-04 15:08:41 +0900

    fix: 프로그래머스 삭제

--- update_stats.py
+++ update_stats.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 USERNAME_BOJ = "sunyu0216"      # 백준/solved.ac 아이디
-USERNAME_PROG = "sunyu0216@naver.com"     # 프로그래머스 닉네임
+# USERNAME_PROG = "sunyu0216@naver.com"     # 프로그래머스 닉네임
 README_PATH = "README.md"
 
 # 백준 티어 숫자 -> 이름 매핑
@@ -32,7 +32,7 @@ def fetch_boj_stats():
     }
 
 # 프로그래머스 통계 가져오기
-def fetch_programmers_stats():
+"""def fetch_programmers_stats():
     url = f"https://school.programmers.co.kr/users/{USERNAME_PROG}/challenges"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
@@ -49,7 +49,7 @@ def fetch_programmers_stats():
         "solved": solved,
         "rank": rank,
         "recent": recent
-    }
+    }"""
 
 # README 업데이트
 def update_readme(boj_stats, prog_stats):
@@ -67,22 +67,22 @@ def update_readme(boj_stats, prog_stats):
     content = content.split(start_boj)[0] + start_boj + "\n" + new_boj + content.split(end_boj)[1]
 
     # 프로그래머스 통계
-    start_prog = "<!-- PROGRAMMERS:START -->"
+    '''start_prog = "<!-- PROGRAMMERS:START -->"
     end_prog = "<!-- PROGRAMMERS:END -->"
     recent_list = "\n".join([f"  - {q}" for q in prog_stats['recent']])
     new_prog = f"""
-**프로그래머스 현황**
-- 푼 문제 수: {prog_stats['solved']}
-- 랭크: {prog_stats['rank']}
-- 최근 풀이:
-{recent_list}
-"""
-    content = content.split(start_prog)[0] + start_prog + "\n" + new_prog + content.split(end_prog)[1]
+      **프로그래머스 현황**
+      - 푼 문제 수: {prog_stats['solved']}
+      - 랭크: {prog_stats['rank']}
+      - 최근 풀이:
+      {recent_list}
+      """
+    content = content.split(start_prog)[0] + start_prog + "\n" + new_prog + content.split(end_prog)[1]'''
 
     with open(README_PATH, "w", encoding="utf-8") as f:
         f.write(content)
 
 if __name__ == "__main__":
     boj_stats = fetch_boj_stats()
-    prog_stats = fetch_programmers_stats()
-    update_readme(boj_stats, prog_stats)
+    #prog_stats = fetch_programmers_stats()
+    update_readme(boj_stats)#, prog_stats)
