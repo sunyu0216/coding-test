2ddb4385d1239cc81f971457ef734d7d4cb40c9b
seonyu <sunyu0216@naver.com>
2026-01-29 12:52:29 +0900

    [docs] 주석 삭제

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,39 +1,27 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
 import java.util.Scanner;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+/**
+ * 
+ * @author seonyu
+ *
+ * 입력
+ * 반복문 10번 -> 
+ * 			테스트케이스번호-testcase, 
+ * 			8개의 데이터-ArrayList<> password
+ * 
+ * 반복문1 {이번에 뒤로가는 숫자가 0이 되거나 0보다 작아질 경우 프로그램 종료
+ * 	     뒤로가는 숫자-lastNumber
+ *     반복문2 {사이클 담당 반복문, 총 5번 반복}
+ * }
+ * 
+ * 출력
+ * 공백을 기준으로 출력하기 위해 stream 활용 -> int Arraylist를 String으로 변환
+ * stream().map(숫자를 문자열로 변환).collect(중간에 공백을 넣어 하나의 문자여롤 합치기)
  */
+
 class Solution
 {
 	public static void main(String[] args) {
